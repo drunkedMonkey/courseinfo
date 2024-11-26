f12e91f8f743109c72ddc4c843d9dc213d60c205
Jose Ibañez <appibanez@gmail.com>
2024-11-26 18:59:59 +0100

    implement map to render array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
-import Course from './components/Course'
+import Course from "./components/Course";
+import ReactDOM from "react-dom/client";
 
-const App = ()=>{
-  const course = {
-    name: 'Half Stack application development',
-    parts:[
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Midelwares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,7 +1,12 @@
 const Total  = (props) => {
+
+  const totalExercises = props.parts.reduce((total, part)=>{
+    return total + part.exercises;
+  },0);
+
   return (
     <p>
-      Number of exercises {props.parts.reduce((total, part) => total + part.exercises, 0)}
+      Number of exercises {totalExercises}
     </p>
   )
 }
